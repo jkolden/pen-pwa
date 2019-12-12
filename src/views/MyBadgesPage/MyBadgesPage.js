0f3c86685952f4d2f903a3eb3c4381e99614f440
John Kolden <john.kolden@gmail.com>
2019-12-12 06:27:33 -0800

    auto expanded brakes pit stop

--- src/views/MyBadgesPage/MyBadgesPage.js
+++ src/views/MyBadgesPage/MyBadgesPage.js
@@ -56,6 +56,7 @@ export default function MyBadgesPage(props) {
                         )}
 
                         <Accordion
+                          active="1"
                           activeColor="primary"
                           collapses={[
                             {
